4106a2bcf0d02ea99ae213733bc174c696ff2c25
MaciejMisztal <maciejmisztal@op.pl>
2015-12-11 01:16:30 +0100

    init

---
+++ config.js
@@ -0,0 +1,41 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "traceur",
+  typescriptOptions: {
+    "noImplicitAny": false,
+    "typeCheck": false,
+    "module": "system",
+    "target": "es5",
+    "experimentalDecorators": true,
+    "emitDecoratorMetadata": true
+  },
+  paths: {
+    "npm:*": "jspm_packages/npm/*",
+    "github:*": "jspm_packages/github/*"
+  },
+  packages: {
+    "src": {
+      "defaultExtension": "ts",
+      "meta": {
+        "*.ts": {
+          "loader": "ts"
+        },
+        "*.js": {
+          "loader": "ts"
+        }
+      }
+    }
+  },
+  map: {
+    "knockout": "github:knockout/knockout@3.4.0",
+    "text": "github:systemjs/plugin-text@0.0.4",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.92",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.92",
+    "ts": "github:frankwallis/plugin-typescript@2.4.2",
+    "typescript": "npm:typescript@1.7.3",
+    "github:frankwallis/plugin-typescript@2.4.2": {
+      "typescript": "npm:typescript@1.7.3"
+    }
+  }
+});
---
+++ src/mko-custom.ts
@@ -0,0 +1,15 @@
+import ko from "knockout";
+
+/**
+* Decorator: Indicates that the decorated class will be a custom element.
+* @param selectorName The name of the custom element.
+* @param templateContet Content (html) of element.
+*/
+export function mkoCustom(selectorName: string, templateContet: string) {
+	return function(target: Function) {
+		ko.components.register(selectorName, {
+			template :  templateContet,
+			viewModel: target
+		})
+	};
+};
