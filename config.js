b75adca574ecd6d0fd719ecae41814fcbe09c431
Maciej Misztal <mim@naszVm.mim.pomorze.pl>
2016-02-05 02:37:17 +0100

    update/reduce deps; rename index.ts->indexTs.ts

--- config.js
+++ config.js
@@ -32,11 +32,9 @@ System.config({
   map: {
     "knockout": "github:knockout/knockout@3.4.0",
     "text": "github:systemjs/plugin-text@0.0.4",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.92",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.92",
-    "ts": "github:frankwallis/plugin-typescript@2.4.5",
+    "ts": "github:frankwallis/plugin-typescript@2.6.0",
     "typescript": "npm:typescript@1.7.5",
-    "github:frankwallis/plugin-typescript@2.4.5": {
+    "github:frankwallis/plugin-typescript@2.6.0": {
       "typescript": "npm:typescript@1.7.5"
     }
   }
