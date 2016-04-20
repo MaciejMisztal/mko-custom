7c87691ccce6f2d314d6dc9ef5a995a3e33649a4
maciej (phone works) <maciejmisztal@op.pl>
2016-04-20 16:41:11 +0200

    tddWIP

--- config.js
+++ config.js
@@ -31,11 +31,20 @@ System.config({
 
   map: {
     "knockout": "github:knockout/knockout@3.4.0",
-    "text": "github:systemjs/plugin-text@0.0.4",
-    "ts": "github:frankwallis/plugin-typescript@4.0.1",
-    "typescript": "npm:typescript@1.8.2",
-    "github:frankwallis/plugin-typescript@4.0.1": {
-      "typescript": "npm:typescript@1.8.2"
+    "text": "github:systemjs/plugin-text@0.0.7",
+    "ts": "github:frankwallis/plugin-typescript@4.0.6",
+    "typescript": "npm:typescript@1.8.10",
+    "github:frankwallis/plugin-typescript@4.0.6": {
+      "typescript": "npm:typescript@1.8.10"
+    },
+    "github:jspm/nodelibs-os@0.1.0": {
+      "os-browserify": "npm:os-browserify@0.1.2"
+    },
+    "npm:os-browserify@0.1.2": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
+    },
+    "npm:typescript@1.8.10": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
     }
   }
 });
